a66409450a5ab84c2b1a1e19356b7d8d3da76db8
ahmad okeal <104964555+ahmadokeal@users.noreply.github.com>
2025-01-04 01:39:02 +0200

    i

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 
 
 let btn = document.querySelector('button')
-btn.onclick = function () {
-    alert("h")
+btn.onclick = async function () {
+    let r = await fetch("ahmadOkiel.42web.io/index.php?q='hello php'").then(res => res.json())
+    alert(r[0])
 }
